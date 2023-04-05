305f14d693d55e212627ccc153eb74a8e80928e1
ayushya001 <ayushbravo001@gmail.com>
2023-04-05 22:13:10 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          body: Center(
+            child: Text("Welcome to my first app using flutter"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
